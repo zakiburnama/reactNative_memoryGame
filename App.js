d050d42a68601cef7a2281f5c59be9efa1a9b9dc
zakiburnama <zaki.burnama1017@gmail.com>
2021-12-11 10:04:52 +0700

    Game besar mana tapi hrusu by: pak jos

--- App.js
+++ App.js
@@ -1,132 +1,90 @@
-import React from 'react';
-import type {Node} from 'react';
-import {
-  Alert,
-  Button,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { Component } from 'react';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import { Button, Text, View, Alert } from 'react-native';
+import { extend } from './node_modules/dayjs/index';
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+export default class HelloWorldApp extends Component {
+    state = {
+        text: 'My Text', random: 5,
+        charData: ['A', 'B', 'C', 'D', 'E'],
+        topFirstChar: 0, topSecondChar: 0,
+        botFirstChar: 0, botSecondChar: 0,
 
-const App: () => Node = () => {
-  return (
-    <SafeAreaView style={styles.containerAll}>
-      <StatusBar barStyle="dark-content"/>
-      <View style={styles.titleFooterCont}>
-        <Text style={styles.titleStyle}>
-          Simple Matching Game
-        </Text>
-      </View>
-      <Separator/>
-      <View style={styles.fixToText}>
-        <Button 
-          color='#1261A0'
-          title="1 button"
-          onPress={() => Alert.alert('1 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="2 button"
-          onPress={() => Alert.alert('2 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="3 button"
-          onPress={() => Alert.alert('3 button pressed')}
-        />
-      </View>
-      <View style={styles.fixToText}>
-        <Button 
-          color='#1261A0'
-          title="4 button"
-          onPress={() => Alert.alert('4 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="5 button"
-          onPress={() => Alert.alert('5 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="6 button"
-          onPress={() => Alert.alert('6 button pressed')}
-        />
-      </View>
-      <View style={styles.fixToText}>
-        <Button
-          color='#1261A0'
-          title="7 button"
-          onPress={() => Alert.alert('7 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="8 button"
-          onPress={() => Alert.alert('8 button pressed')}
-        />
-        <Button
-          color='#1261A0'
-          title="9 button"
-          onPress={() => Alert.alert('9 button pressed')}
-        />
-      </View>
-      <View style={styles.fixToText}>
-        <Button
-          color='#072F5F'
-          title="Go Start button"
-          onPress={() => Alert.alert('Go Start button pressed')}
-        />
-      </View>
-      <Separator/>
-      <View style={styles.titleFooterCont}>
-        <Text style={styles.footerStyle}>
-          Tugas 5 MPR - Kelompok Delta 
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-};
+        topButtonText: 'AA',
+        botButtonText: 'AA'
+    }
 
-const styles = StyleSheet.create({
-  containerAll: {
-    justifyContent: 'space-evenly', 
-    flex:1,
-    backgroundColor: '#58CCED',
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginTop: 20,
-  },
-  titleFooterCont: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  titleStyle: {
-    color: '#001540',
-    fontSize: 35,
-    fontFamily: 'sans-serif-medium',
-  },
-  footerStyle: {
-    color: '#001540',
-    fontSize: 15,
-    fontFamily: 'sans-serif-condensed',
-  },
-  separator: {
-    borderWidth: 3,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
+    onPressTop = () => {
 
-export default App;
+        var topValue = this.state.topFirstChar + this.state.topSecondChar;
+        var botValue = this.state.botFirstChar + this.state.botSecondChar;
+
+        
+        if (topValue > botValue) {
+            this.setState({ text: 'Benar' })
+        }
+        else {
+            this.setState({ text: 'Salah' })
+        }
+        this.testRandom()
+    }
+
+    onPressBot = () => {
+        var topValue = this.state.topFirstChar + this.state.topSecondChar;
+        var botValue = this.state.botFirstCHar + this.state.botSecondChar;
+        if (topValue < botValue) {
+            this.setState({ text: 'Benar' })
+        }
+        else {
+            this.setState({ text: 'Salah' })
+        }
+        this.testRandom()
+    }
+
+
+    testRandom = () => {
+        
+        this.setState({
+            topFirstChar: parseInt(Math.random() * 5, 10),
+            topSecondChar: parseInt(Math.random() * 5, 10),
+
+            botFirstChar: parseInt(Math.random() * 5, 10),
+            botSecondChar: parseInt(Math.random() * 5, 10),
+
+            topButtonText: this.state.charData[this.state.topFirstChar] + this.state.charData[this.state.topSecondChar],
+            botButtonText: this.state.charData[this.state.botFirstChar] + this.state.charData[this.state.botSecondChar]
+            //topButtonText: 'AA',
+            //botButtonText: 'CC'
+
+        })
+    }
+
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text> {this.state.text} </Text>
+                <Button
+                    onPress={this.onPressTop}
+
+                    title={this.state.topButtonText}
+                    color="#AABBAA"
+                />
+
+                <Text>{"\n"}</Text>
+
+                <Button
+                    onPress={this.onPressBot}
+
+                    title={this.state.botButtonText}
+                    color="#AABBAA"
+                />
+
+            </View>
+        )
+    }
+}
